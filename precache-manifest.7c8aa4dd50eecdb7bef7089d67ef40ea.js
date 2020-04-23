5fbce9f40e007425d838c70dba2a90ad416a5d13
medo <ostora.dev@gmail.com>
2020-04-23 12:11:04 -0700

    Updates

---
+++ precache-manifest.7c8aa4dd50eecdb7bef7089d67ef40ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e36fdb3772090b5700055c1a2b1d375",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "89228f0d8511f52e209d",
+    "url": "/covid-19/static/css/main.57d24843.chunk.css"
+  },
+  {
+    "revision": "d76b37bcf6fd6b764638",
+    "url": "/covid-19/static/js/2.dc18bcfa.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.dc18bcfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89228f0d8511f52e209d",
+    "url": "/covid-19/static/js/main.c2c6cf50.chunk.js"
+  },
+  {
+    "revision": "49c3c8bf8cc6af0b31da",
+    "url": "/covid-19/static/js/runtime-main.1210cd21.js"
+  }
+]);
